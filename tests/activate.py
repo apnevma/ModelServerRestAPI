cb67bd497dcf027eaad381497112ee8bb8c6a418
apnevma <apnevma@netcompany.com>
2026-01-13 11:23:38 +0200

    removed test scripts - use postman

--- tests/activate.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-models_to_activate = ("saved_model", "capacitor_fit_model_savedmodel", "rf_model")
-
-for model in models_to_activate:
-    resp = requests.post(f"http://localhost:8086/activate/{model}")
-    print(resp.json())
--- tests/deactivate.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-models_to_deactivate = ("saved_model", "fake_model")
-
-for model in models_to_deactivate:
-    resp = requests.post(f"http://localhost:8086/deactivate/{model}")
-    print(resp.json())
